585e9873da22eb12479e315f90f5f3841011d2d4
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-22 11:13:29 +0300

    Completed assignmnet

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,37 @@
 // add code snippets from README
+let state;
+
+function reducer(state = {
+    count: 0
+}, action) {
+    switch (action.type) {
+        case "counter/increment":
+            return {
+                count: state.count + 1
+            };
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render();
+}
+
+function render() {
+    const counter = document.getElementById('button')
+    counter.textContent = state.count
+}
+
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    dispatch({
+        type: 'counter/increment'
+    })
+})
+
+dispatch({
+    type: "@@INIT"
+});
